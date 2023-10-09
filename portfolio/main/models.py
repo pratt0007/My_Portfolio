50697233fe8a7eb4a5122953db93828596559a52
Pratyush Nagpal <h83pratyush@gmail.com>
2023-10-10 02:45:20 +0530

    View func

--- portfolio/main/models.py
+++ portfolio/main/models.py
@@ -29,4 +29,15 @@ class UserProfile(models.Model):
     
 
 class contactprofile(models.Model):
+
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(verbose_name="Name",max_length=100)
+    email = models.EmailField(verbose_name="Email")
+    message = models.TextField(verbose_name="Message")
+
+    def __str__(self):
+        return f'{self.name}'
+
+
     
--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -1,3 +1,18 @@
+from typing import Any
 from django.shortcuts import render
+from django.contrib import messages
 
+from .models import( UserProfile )
+
+
+from django.views import generic
 # Create your views here.
+
+class IndexView(generic.TemplateView):
+    template_name = "main/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        
+
