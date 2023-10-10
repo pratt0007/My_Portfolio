5723df46de5e6dacb161ead4eba1eb40f5f1e5b2
Pratyush Nagpal <h83pratyush@gmail.com>
2023-10-10 21:56:19 +0530

    update

--- portfolio/main/models.py
+++ portfolio/main/models.py
@@ -39,12 +39,31 @@ class contactprofile(models.Model):
     def __str__(self):
         return f'{self.name}'
 
+class Media(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Media Files'
+        verbose_name = 'Media'
+        ordering = ["name"]
+	
+    image = models.ImageField(blank=True, null=True, upload_to="media")
+    url = models.URLField(blank=True, null=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    is_image = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if self.url:
+            self.is_image = False
+        super(Media, self).save(*args, **kwargs)
+    def __str__(self):
+        return self.name
+
 class portfolio(models.Model):
 
-    # class Meta:
-    #     verbose_name_plural = 'Portfolio Profiles'
-    #     verbose_name = 'Portfolio'
-    #     ordering = ["name"]
+    class Meta:
+        verbose_name_plural = 'Portfolio Profiles'
+        verbose_name = 'Portfolio'
+        ordering = ["name"]
     date = models.DateTimeField(blank=True, null=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     description = models.CharField(max_length=500, blank=True, null=True)
@@ -64,5 +83,16 @@ class portfolio(models.Model):
     def get_absolute_url(self):
         return f"/portfolio/{self.slug}"
 
+class certificate(models.Model):
+    class Meta:
+        verbose_name_plural = 'Certificates'
+        verbose_name = 'Certificate'
 
-    
+    date = models.DateTimeField(blank=True, null=True)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    title = models.CharField(max_length=200, blank=True, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
