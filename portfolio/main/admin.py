cbee761a4d79f1ab4cd5b214661a07cb53b0e170
Pratyush Nagpal <h83pratyush@gmail.com>
2023-10-10 22:28:08 +0530

    update

--- portfolio/main/admin.py
+++ portfolio/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import (UserProfile , skill)
+from .models import (UserProfile , skill , Media , certificate, contactprofile  , portfolio)
 # Register your models here.
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
@@ -9,3 +9,16 @@ class UserProfileAdmin(admin.ModelAdmin):
 class SkillAdmin(admin.ModelAdmin):
     list_display = ('id' , 'name' , 'score')
 
+
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('id','name','is_active')
+    readonly_fields = ('slug',)
+
+@admin.register(Certificate)
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = ('id','name')
