9aea0c0aa9619d9723eaf19ba1ced26c8d1f7e38
Pratyush Nagpal <h83pratyush@gmail.com>
2023-10-11 16:41:27 +0530

    Update

--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -2,7 +2,7 @@ from typing import Any
 from django.shortcuts import render
 from django.contrib import messages
 
-from .models import( UserProfile, portfolio , certificate , contactprofile)
+from .models import( UserProfile, portfolio , certificate , contactprofile , Blog , Testimonial)
 
 from .forms import contactform
 
@@ -14,11 +14,25 @@ class IndexView(generic.TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+		testimonials = Testimonial.objects.filter(is_active=True)
+		certificates = certificate.objects.filter(is_active=True)
+		blogs = Blog.objects.filter(is_active=True)
+		portfolio = portfolio.objects.filter(is_active=True)
+		
+		context["testimonials"] = testimonials
+		context["certificates"] = certificates
+		context["blogs"] = blogs
+		context["portfolio"] = portfolio
+		return context
     
 
 class PortfolioDetailView(generic.DetailView):
 	model = portfolio
 	template_name = "main/portfolio-detail.html"
+
+
+
 class PortfolioView(generic.ListView):
 	model = portfolio
 	template_name = "main/portfolio.html"
@@ -37,3 +51,15 @@ class ContactView(generic.FormView):
 		messages.success(self.request, 'Thank you. We will be in touch soon.')
 		return super().form_valid(form)
 
+
+class BlogView(generic.ListView):
+	model = Blog
+	template_name = "main/blog.html"
+	paginate_by = 10
+	
+	def get_queryset(self):
+		return super().get_queryset().filter(is_active=True)
+
+class BlogDetailView(generic.DetailView):
+	model = Blog
+	template_name = "main/blog-detail.html"
